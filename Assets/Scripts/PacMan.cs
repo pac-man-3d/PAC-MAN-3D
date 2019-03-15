217acefe50dc07d2697ba35c87c3958a5027cbc6
lars.pelz@gmx.net <lars.pelz@gmx.net>
2019-03-15 15:13:15 +0100

    Pacman lässt sich mit den Pfeiltasten steuern

--- Assets/Scripts/PacMan.cs
+++ Assets/Scripts/PacMan.cs
@@ -1,74 +1,46 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using System.Collections;
 
-public class Bewegen : MonoBehaviour
-{
+public class PacMan : MonoBehaviour {
 	
-	//this.transform.position.Set(1f, 0f, 0f);
-	//this.transform.position = new Vector3(1f, 0f, 0f);
-
-	public float Geschwindigkeit = 10f;
-	public float Richtung = Vector3(1f, 0f, 0f);
-
-	float aktuelleGeschwindigkeit = 0;
-    //public float Geschwindigkeit;
-    
-    Vector3 richtung = Vector3.zero;
-
+	public float moveSpeed;
+	public float jumpForce;
+	public float mouseSpeed;
+	
+    // Start is called before the first frame update
     void Start()
     {
+        
     }
 
-    void Update()
-    {
-        if (EingabeUeberpruefen())
-        {
-            if (aktuelleGeschwindigkeit < Geschwindigkeit)
-            {
-                aktuelleGeschwindigkeit += Geschwindigkeit;
-            }
-            this.transform.position += richtung * aktuelleGeschwindigkeit;
-        }
-        else
-        {
-            aktuelleGeschwindigkeit = 0f;
-        }
-    }
-
-    private bool EingabeUeberpruefen()
-    {
-        bool eingabe = false;
-        //float x = 0f;
-        //float z = 0f;
-      
-      
-        // Input.GetKey gibt true zurück wenn
-        // die gefragte Taste gedrückt ist
-      
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            x++;
-            eingabe = true;
-        }
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            x--;
-            eingabe = true;
-        }
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            z++;
-            eingabe = true;
-        }
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            z--;
-            eingabe = true;
-        }
-        richtung = Vector3(x, 0f, z);
-        return eingabe;
-    }
-	
-							
+    void FixedUpdate ()
+	{
+		// Spieler-Position im Sinne der betätigten Kontrollenn aktualisieren
+		GetComponent<Rigidbody>().velocity =
+			transform.right * Input.GetAxis ("Horizontal") * moveSpeed + 
+			transform.forward * Input.GetAxis ("Vertical") * moveSpeed + 
+			Vector3.up * GetComponent<Rigidbody>().velocity.y;
+		GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+		
+		// Sprungfähigkeit
+		RaycastHit hit;
+		Debug.DrawRay(transform.position + Vector3.up * 0.1f, Vector3.down * 0.2f, Color.yellow);
+		// wenn Spieler auf dem Boden steht...
+		if (Physics.Raycast (transform.position + Vector3.up * 0.1f, Vector3.down, out hit, 0.2f))
+		{
+			// und die Leertaste gegrückt ist
+			if (Input.GetKeyDown ("space"))
+			{
+				// eine Kraft nach oben ausüben
+				GetComponent<Rigidbody> ().AddForce (Vector3.up * jumpForce);
+			}
+		}
+		
+		// Spieler-Drehung im Sinne der Mausbewegung aktualisieren
+		transform.Rotate (0, Input.GetAxisRaw ("Mouse X") * mouseSpeed * Time.fixedDeltaTime, 0);
+		
+		// Kamera mit Spieler drehen (Position wird automatisch aktualisiert)
+		Camera.main.transform.Rotate(new Vector3 (-Input.GetAxisRaw ("Mouse Y") * mouseSpeed * Time.fixedDeltaTime, 0, 0));
+	}
 }
