9a3cfd69e000ffd2d422c4336c3d964fa9b0b49a
timmiber <timmiber@gmx.de>
2019-04-10 13:09:03 +0200

    Pacman's Rotation

--- Assets/Scripts/DrehungPacManTest.cs
+++ Assets/Scripts/DrehungPacManTest.cs
@@ -13,13 +13,46 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        	if(Input.GetKeyDown("d"))
+        //var grad : int = 90;
+		//var i : float;
+		//var factor : int = 100;
+		if(Input.GetKeyDown("d"))
 		{
-			transform.Rotate (0,90,0);
+			for(var i = 0; i < 90; i++) //soll in die eine Richtung drehen
+				{
+					transform.Rotate (0, i * Time.deltaTime * 100, 0);
+				}
 		}
 		if(Input.GetKeyDown("a"))
 		{
 			transform.Rotate(0,-90,0);
 		}
     }
+	
+	
+	/*
+	function OnMouseDown()
+	{
+
+		switch(mode)
+		{
+			case 1:
+
+				for(i = 0; i < grad; i++) //soll in die eine Richtung drehen
+				{
+					transform.Rotate (0, i * Time.deltaTime * factor, 0);
+				}
+				break;
+			case 2:
+
+				for(i = 35; i > grad-grad; i--)
+				{
+					transform.Rotate (0, i * Time.deltaTime * factor, 0); //soll in die andere Richtung drehen
+				}
+				break;
+		}
+
+	}
+*/
+
 }
