a7f667bf7c5456c0c011a72b922f148db1360bc7
Tekkishi <khainguyen300@gmail.com>
2019-04-11 09:15:14 +0200

    Kameraverfolgung fertig

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,24 +5,36 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform target;
-    
-    
     public float smoothSpeed = 0.2f;
     public Vector3 offset;
 
-    void LateUpdate()
+    // Update is called once per frame
+    void FixedUpdate()  
     {
         Vector3 optimalePosition = target.position + offset;
-        Vector3 smoothedPosition = Vector3.Lerp (transform.position, optimalePosition,smoothSpeed);
+        Vector3 smoothedPosition = Vector3.Lerp (transform.position, optimalePosition, smoothSpeed);
         transform.position = smoothedPosition;
-        //Vector3 rot = target.eulerAngles;
-       // rot = new Vector3(rot.x,rot.y-90,rot.z);
-        //transform.rotation = Quaternion.Euler(rot); 
-       
+
+        transform.LookAt(target);
+        transform.rotation = Quaternion.Euler (target.rotation.eulerAngles.x+40,target.rotation.eulerAngles.y-90, target.rotation.eulerAngles.z);
         
-    }
+        if(transform.rotation.eulerAngles.y>= 180 && transform.rotation.eulerAngles.y <181 )
+        {
+             offset = new Vector3(0,200,150);
+        }
+        if(transform.rotation.eulerAngles.y>= 90 && transform.rotation.eulerAngles.y <91 )
+        {
+            offset = new Vector3 (-150,200,0);
+        }
+        if(transform.rotation.eulerAngles.y>= 0 && transform.rotation.eulerAngles.y <1 )
+        {
+            offset = new Vector3 (0,200,-150);
+        }
+        if(transform.rotation.eulerAngles.y >= 270 && transform.rotation.eulerAngles.y <271)
+        {
+           offset = new Vector3 (150,200,0);
+        }
     
 }
-    
- 
-     
+
+}   
--- Assets/Scripts/CameraTest.cs
+++ Assets/Scripts/CameraTest.cs
@@ -9,7 +9,7 @@ public class CameraTest : MonoBehaviour
     public Vector3 offset;
 
     // Update is called once per frame
-    void FixedUpdate()
+    void FixedUpdate()  
     {
         Vector3 optimalePosition = target.position + offset;
         Vector3 smoothedPosition = Vector3.Lerp (transform.position, optimalePosition, smoothSpeed);
@@ -17,5 +17,24 @@ public class CameraTest : MonoBehaviour
 
         transform.LookAt(target);
         transform.rotation = Quaternion.Euler (target.rotation.eulerAngles.x+40,target.rotation.eulerAngles.y-90, target.rotation.eulerAngles.z);
+        
+        if(transform.rotation.eulerAngles.y>= 180 && transform.rotation.eulerAngles.y <181 )
+        {
+             offset = new Vector3(0,200,150);
+        }
+        if(transform.rotation.eulerAngles.y>= 90 && transform.rotation.eulerAngles.y <91 )
+        {
+            offset = new Vector3 (-150,200,0);
+        }
+        if(transform.rotation.eulerAngles.y>= 0 && transform.rotation.eulerAngles.y <1 )
+        {
+            offset = new Vector3 (0,200,-150);
+        }
+        if(transform.rotation.eulerAngles.y >= 270 && transform.rotation.eulerAngles.y <271)
+        {
+           offset = new Vector3 (150,200,0);
+        }
+    
 }
+
 }   
