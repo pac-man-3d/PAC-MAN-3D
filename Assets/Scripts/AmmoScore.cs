a8c24c64f15b5098e0bccde9bf61c2de418aafa0
mrvnplz <marvin.pilz01@gmx.de>
2019-04-23 17:51:09 +0200

    Ammo und Münz Score fertig

---
+++ Assets/Scripts/AmmoScore.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AmmoScore : MonoBehaviour
+{ 
+public Text countText;
+private int count;
+
+// Start is called before the first frame update
+void Start()
+{
+    count = 0;
+    SetCountText();
+}
+
+// Update is called once per frame
+void Update()
+{
+
+}
+
+private void OnTriggerEnter(Collider other)
+{
+    if (other.gameObject.tag == "Ammo")
+    {
+        count = count + 1;
+        SetCountText();
+    }
+}
+void SetCountText()
+{
+    countText.text = "Ammo: " + count.ToString();
+}
+}
+
