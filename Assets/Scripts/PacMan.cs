5f08c23a92c3049d2e1a7435ffa07d56551807c3
Tekkishi <khainguyen300@gmail.com>
2019-05-07 20:39:51 +0200

    git pull

--- Assets/Scripts/PacMan.cs
+++ Assets/Scripts/PacMan.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PacMan : MonoBehaviour {
-	public float moveSpeed;
+    public float moveSpeed = 5.0f;
 
-	void Start(){}
+
+    void Start(){}
 	void Update()
 	{
-		Vector3 pos = Vector3.forward * 5.0f * Time.deltaTime*10;
+		Vector3 pos = Vector3.forward * moveSpeed * Time.deltaTime*10;
 		transform.Translate(pos);
-        //transform.Translate(moveSpeed*Input.GetAxis("Horizontal")*Time.deltaTime,0f,moveSpeed*Input.GetAxis("Vertical")*Time.deltaTime);
-		if(Input.GetKeyDown("d"))
+        checkWall();
+ 
+        if (Input.GetKeyDown("d"))
 		{		
 			transform.Rotate (0, 90, 0);		
 		}
@@ -20,6 +22,19 @@ public class PacMan : MonoBehaviour {
 			transform.Rotate(0,-90,0);
 		}
 	}
+    void checkWall()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 50f))
+        {
+            moveSpeed = 0;
+            //Debug.Log(hit.transform.name);
+        }
+        else
+        {
+            moveSpeed = 5.0f;
+        }
+    }
 }
 
 
